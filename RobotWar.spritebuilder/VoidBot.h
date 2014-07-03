188ab36e4af0481b30ed8b53d270f5219c4e06c9
Dat Do <DatDo@Dats-MacBook-Pro.local>
2014-07-03 11:12:46 -0400

    create voidbot

---
+++ RobotWar.spritebuilder/VoidBot.h
@@ -0,0 +1,13 @@
+//
+//  VoidBot.h
+//  RobotWar
+//
+//  Created by Dat Do on 7/3/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Robot.h"
+
+@interface VoidBot : Robot
+
+@end
